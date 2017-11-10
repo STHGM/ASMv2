bde4e350a2b526fd2eb8df0b930a7d64dfe9ffa0
Rodrigo Moretto <rodrigo-moretto@hotmail.com>
2017-11-10 18:51:27 -0200

    Modificação na Main
    Main agora inicia com a tela de Login

--- ASM/Automatic Storage Manager/src/Principal/Main.java
+++ ASM/Automatic Storage Manager/src/Principal/Main.java
@@ -1,9 +1,11 @@
 package Principal;
 
 import Interfaces.inicial;
+import Interfaces.login;
 
 public class Main{
-	public static void main(String arg[]){
-                new inicial().setVisible(true);
-	}
+    public static void main(String arg[]){
+        new login().setVisible(true);
+        //new inicial().setVisible(true);
+    }
 }
